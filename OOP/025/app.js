7c1faa717368306e1ccf70e8ff7b3140a03a031d
sergei <sverkeenko1303@gmail.com>
2022-10-23 20:32:54 +0300

    project

---
+++ OOP/025/app.js
@@ -0,0 +1,37 @@
+// function myl() {
+//     let sum = 0;
+//     return () => {
+//         return ++sum
+//     }
+// }
+// const p = myl()
+// console.log(p());
+// console.log(p());
+// console.log(p());
+// console.log(p());
+// console.log(p());
+
+// class Zam {
+//     sum = 0;
+//     myl() {
+//         return ++this.sum
+//     }
+// }
+
+// const zam = new Zam();
+// console.log(zam.myl());
+// console.log(zam.myl());
+// console.log(zam.myl());
+
+// const obj = {
+//     count: 0,
+//     sum: function () {
+//         return ++this.count
+//     }
+// }
+// console.log(obj.sum());
+
+function fact(n) {
+    if (n == 1) return 1
+    return n * fact(n - 1)
+}
