623e40a3e7ff11bf10fb2e427495926e9c1d6b17
sergei <sverkeenko1303@gmail.com>
2022-12-01 22:55:43 +0300

    js

---
+++ node js/s003/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { getEnviroment, getEnviromentById } = require('./services/enviroment.services')
+const app = express();
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params
+    let result = getEnviromentById(id)
+    res.send(result)
+})
+
+app.post('/', (req, res) => {
+    res.send('hey POST')
+})
+
+app.listen(8000, () => console.log('Servak ready'))
--- node js/server/app.js
+++ node js/server/app.js
@@ -1,7 +1,47 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-app.get('/', (req, res) => res.send('Hello, NodeJS'))
-app.listen(port, () => {
-    console.log('server is run')
+app.get('/',(req,res)=>{
+    res.send('hello')
 })
+app.listen(8000,()=>console.log('server on'))
+
+
+
+// const storage = [
+//     {
+//         id: 1,
+//         title: 'часы',
+//         price: 333,
+//         count: 3
+//     },
+//     {
+//         id: 2,
+//         title: 'смартфон',
+//         price: 33,
+//         count: 3
+//     },
+//     {
+//         id: 3,
+//         title: 'тв',
+//         price: 353,
+//         count: 1
+//     },
+// ];
+
+// const id = 2;
+// function binSearch(storage, id) {
+//     let one = 0;
+//     let kon = storage.length;
+//     let midl = Math.round((one + kon) / 2);
+//     let res
+//     for (let i = 0; i < kon; i++) {
+//         if (midl === id) {
+//             res = storage[id - 1];
+//         } else {
+//             kon = midl
+//         }
+//     }
+//     return res;
+// }
+
+// console.log(binSearch(storage, id)); 
