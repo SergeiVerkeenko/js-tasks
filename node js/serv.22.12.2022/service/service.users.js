a2059442b016a926943c5f0c44a1d2952decc025
sergei <sverkeenko1303@gmail.com>
2022-12-22 22:40:23 +0300

    envinroment api server OOP

---
+++ node js/serv.22.12.2022/service/service.users.js
@@ -0,0 +1,16 @@
+const { getUsersDB, getUsersByIdDB } = require('../repository/repository.users')
+
+
+async function getUsers() {
+    const users = await getUsersDB()
+    if (!users.length) throw new Error('DB is empty')
+    return users
+}
+
+async function getUsersById(id) {
+    const users = await getUsersByIdDB(id)
+    // if (!users.length) throw new Error('DB is empty')
+    return users
+}
+
+module.exports = { getUsers, getUsersById }
